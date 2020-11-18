16983f67baf05f6951d611e51b4cf51dea4c0f30
ohgyeongseok <73566235+ohgyeongseok@users.noreply.github.com>
2020-11-18 16:48:30 +0900

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,3 +1,26 @@
 $(function(){
-  console.log("s")
-})
+  //script 입력영역
+  $("nav ul li a,.back_to_top a,.footer_top a,a.button").click(function(e){
+    // console.log($(this.hash).offset().top)
+    var thisElem = $(this.hash);
+    $("html,body").stop();
+    $("html,body").animate({scrollTop : thisElem.offset().top},1500);
+    //e.preventDefault();
+    return false;
+  });
+
+  /*scroll 상단 이동버튼 노출*/
+  $(".back_to_top").css("opacity","0")
+  $(window).scroll(function(){
+    // console.log($(this).scrollTop());
+    if( $(this).scrollTop() > 0 ){
+      $(".back_to_top").fadeIn("slow");
+    }else{
+      $(".back_to_top").fadeOut("slow");
+    }
+  });
+
+
+
+
+});
